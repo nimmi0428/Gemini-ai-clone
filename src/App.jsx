97516c0195c214d0ccca0e31d731df3830c57b86
Nimmi Singh <nimmisingh0428@gmail.com>
2025-05-13 14:17:29 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Sidebar from "./components/sidebar/sidebar"
+import Main from "./components/main/Main"
+
+function App() {
+ 
+
+  return (
+    <>
+      <Sidebar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
